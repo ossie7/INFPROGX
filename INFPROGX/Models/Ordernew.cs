fc7bcf553c5e31e2fdcd0b9b4a8436529e3ba6c9
liyu91 <liyu_1991@live.com>
2013-03-01 23:33:47 +0100

    dit worden de nieuwe corecte model classes met met de bijhorende display en validation attributes validation attributes

---
+++ INFPROGX/Models/Ordernew.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+//dit hoort te werken maar er mag geen 2x zelfde Order bestaan anders gaat het niet builden
+namespace INFPROGX.Models
+{ /*
+    public class Order
+    {
+        // dit is een id dus required hoeft niet aangezien alles met id goed wordt herkent door entity
+        public int OrderId { get; set; }
+        public int UserId { get; set; }
+        public DateTime Date { get; set; }
+    }
+ 
+    public class OrderDbContext : DbContext
+    {
+        public DbSet<Order> Order { get; set; }
+    }*/
+
+}
---
+++ INFPROGX/Models/Shippingnew.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+//dit hoort te werken maar er mag geen 2x zelfde shipping bestaan anders gaat eht neit builden
+namespace INFPROGX.Models
+{ /*
+    public class Shipping
+    {
+        public int ShippingId { get; set; }
+        public int OrderId { get; set; }
+
+        public DateTime Date { get; set; }
+        // kan beter met decimal worden gedaan aangezien het preciezer is
+        //hieronder zorgt voor betere afbaking ook bij ui door middel van jquiry
+        //euroteken/dollar etc.. erbij
+        [Range(1, 1000000)]
+        [DataType(DataType.Currency)]
+        public float Cost { get; set; }
+        public string Type { get; set; }
+    }
+ 
+    public class ShippingDbContext : DbContext
+    {
+        public DbSet<Shipping> Shipping { get; set; }
+    }*/
+}
