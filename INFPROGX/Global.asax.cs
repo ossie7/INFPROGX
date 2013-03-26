01c8944a4bd9020d005258970a2feeba076927ae
liyu91 <liyu_1991@live.com>
2013-03-26 21:12:18 +0100

    Seeding for database

--- INFPROGX/Global.asax.cs
+++ INFPROGX/Global.asax.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -16,6 +17,8 @@ namespace INFPROGX
     {
         protected void Application_Start()
         {
+            //for seeding werkt effe uitgeschakeld door tijdelijke db problemen niet mijn schuld:P
+           // Database.SetInitializer(new Models.StoreDbInitializer());
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
---
+++ INFPROGX/Models/StoreDbInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace INFPROGX.Models
+{
+    //seed db
+    public class StoreDbInitializer : DropCreateDatabaseAlways<ShopDbContext>
+    {
+        protected override void Seed(ShopDbContext context)
+        {
+       //     public DbSet<AbstractProduct> Product   { get; set; }
+      //  public DbSet<Order>           Order     { get; set; }
+      //  public DbSet<OrderLine>       OrderData { get; set; }
+       // public DbSet<Shipping>        Shipping  { get; set; }
+            
+
+            //vb.
+          //  context.Order.Add(new Order { OrderId = 1, UserId = 1 });
+            //etc...
+        }
+
+    }
+}
